55483e4e42aa5ac9e5cbeb9d4619834300a1431d
yuliya <yu227591@gmail.com>
2025-03-03 18:32:51 +0300

    add README.md

---
+++ src/main/java/ru/yandex/practicum/srpint4app/Srpint4AppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.srpint4app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpint4AppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Srpint4AppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/yandex/practicum/srpint4app/Srpint4AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.srpint4app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srpint4AppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
